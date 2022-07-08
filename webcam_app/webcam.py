80539e37841b71f119bd713c2811f83993183e5c
Kurama2d <kuramatw@gmail.com>
2022-07-08 16:48:51 +0300

    working_version

--- webcam_app/webcam.py
+++ webcam_app/webcam.py
@@ -1,16 +1,36 @@
 import cv2,time
 
+first_frame = None
 video = cv2.VideoCapture(0)
 
 while  True:
     check, frame = video.read()
 
-    gray  =  cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #time.sleep(3)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (9,9), 0)
+    
+    if first_frame is None:
+        first_frame = gray
+        continue
+
+    delta_frame = cv2.absdiff(first_frame, gray)
+    thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.dilate(thresh_frame, None, iterations=4)
+
+    (cnts, _) =  cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in cnts:
+        if cv2.contourArea (contour) < 1000:
+            continue
+        
+        (x,y,w,h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (221, 160, 221), 2)
+
     cv2.imshow('Capturing', gray)
-
-    key = cv2.waitKey(1000)
-
+    cv2.imshow('Delta_frame', delta_frame)
+    cv2.imshow('Threshold Frame', thresh_frame)
+    cv2.imshow('Color Frame', frame)
+    key = cv2.waitKey(1)
     if key == ord('q'):
         break
 
