3cf433fe044a1c9ab94931903adcec6d97574d51
Kurama2d <kuramatw@gmail.com>
2022-07-08 17:35:28 +0300

    Finished

--- webcam_app/webcam.py
+++ webcam_app/webcam.py
@@ -1,13 +1,18 @@
-import cv2,time
+import cv2
+from datetime import datetime
+import pandas
 
 first_frame = None
+status_list = [None,  None]
+times = []
 video = cv2.VideoCapture(0)
+df = pandas.DataFrame(columns=['Start', 'End'])
 
 while  True:
     check, frame = video.read()
-
+    status = 0
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (9,9), 0)
+    gray = cv2.GaussianBlur(gray, (15,15), 0)
     
     if first_frame is None:
         first_frame = gray
@@ -20,20 +25,34 @@ while  True:
     (cnts, _) =  cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
-        if cv2.contourArea (contour) < 1000:
+        if cv2.contourArea (contour) < 10000:
             continue
-        
+        status = 1
         (x,y,w,h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x,y), (x+w,y+h), (221, 160, 221), 2)
+
+    status_list.append(status)
+    if  status_list[-1]  == 1 and status_list[-2]  == 0:
+        times.append(datetime.now())
+
+    if  status_list[-1]  == 0 and status_list[-2]  == 1:
+        times.append(datetime.now())
 
     cv2.imshow('Capturing', gray)
     cv2.imshow('Delta_frame', delta_frame)
     cv2.imshow('Threshold Frame', thresh_frame)
     cv2.imshow('Color Frame', frame)
     key = cv2.waitKey(1)
+
     if key == ord('q'):
+        if status == 1:
+            times.append(datetime.now())
         break
 
+for i in range(0, len(times), 2):
+    df = df.append({'Start':times[i], 'End':times[i+1]}, ignore_index=True)
+
+df.to_csv('Times.csv')
 
 video.release()
 cv2.destroyAllWindows
